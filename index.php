be8b2ffc3e2f6b5654852e444d726498d8584b1f
Jbarbosa369 <jeovana.nascimento@aluno.senai.br>
2025-11-26 10:12:21 -0500

    Servidor

--- index.php
+++ index.php
@@ -10,5 +10,19 @@ if ($conexao->connect_error) {
     die("Erro na conexão: " . $conexao->connect_error);
 }
 
-echo "Conectado";
+echo "Conectado"."<br>";
+
+function selecionarTabela($nomeDaTabela) {
+    global $conexao;
+    $sql = "SELECT * FROM  $nomeDaTabela";
+    $resultado = $conexao-> query($sql);
+    if ($resultado->num_rows > 0) {
+        while ($linha = $resultado -> fetch_assoc()){
+            echo $linha['nome'];
+        }
+    } else{ 
+        echo "Tabela não encontrada !!!";
+}
+}
+selecionarTabela("usuarios")
 ?>
